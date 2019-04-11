c500d0cb5f677d8552a7c468714a9a8c6fd5c8f4
Hemanth P <phemanth1987@gmail.com>
2019-04-11 19:02:10 +0530

    Initial commit

---
+++ src/test/java/com/basic/tests/FirstSeleniumScript.java
@@ -0,0 +1,44 @@
+package com.basic.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumScript {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriver driver = null;
+		String browser = "chrome";
+		
+		if(browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+			driver = new ChromeDriver();	
+		}
+		else if(browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/chromedriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		
+		String url = "http://www.google.com";
+		String actualTitle = "";
+		String expTitle = "Google"; 
+		
+		//Launch browser and open URL
+		driver.get(url);
+		//Thread.sleep(10000);
+		actualTitle = driver.getTitle();
+		
+		
+		//Compare Actual and Expected
+		if(actualTitle.equals(expTitle))
+			System.out.println("Test Passed!");
+		else
+			System.out.println("Test Failed!!");
+		
+		driver.close();
+
+	}
+
+}
